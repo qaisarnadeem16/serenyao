086eb42989b8753c74d0dc9fa1026150366e45c6
qaisarnadeem16 <qaisar.nadeem.gill17@gmail.com>
2025-12-08 21:04:07 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -157,6 +157,7 @@ export default function Home() {
         buttonLabel="Learn More"
         buttonHref="/about-us"
       />
+      
 
       {/* Featured Collections Section */}
       <ProductShowcaseSection
